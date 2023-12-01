6cea70cbd82a2e14e713e5930d663999a4c1a3cb
Arsen Beglaryan <arsen.beglarian@gmail.com>
2023-12-01 05:58:32 +0000

    Transform test completed on a real data

---
+++ models/userbmerge.sql
@@ -0,0 +1,13 @@
+{{
+
+config(
+    materialized='table'
+)
+
+}}
+
+
+select ev.event_name, ao.platform from `datatestproj-404419.userbase.app_opens` ao
+full join `datatestproj-404419.userbase.events` ev
+on ao.session_skey = ev.session_skey
+where ev.event_name is not null
