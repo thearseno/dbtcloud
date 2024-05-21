cab396ce78c165e2fc33903f6f8e01f8eaf08687
Arsen Beglaryan <arsen.beglarian@gmail.com>
2024-05-21 17:41:13 +0000

    where clause changesd

--- models/userbmerge.sql
+++ models/userbmerge.sql
@@ -10,4 +10,3 @@ config(
 select ev.event_name, ao.platform, ao.user_skey from `datatestproj-404419.userbase.app_opens` ao
 full join `datatestproj-404419.userbase.events` ev
 on ao.session_skey = ev.session_skey
-where ev.event_name is not null
